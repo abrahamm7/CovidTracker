429a0acbc7261ef04ee5b8843a3a877b0562cef2
Abraham Morillo <abrahammorillo777@gmail.com>
2020-04-19 17:07:43 -0400

    .

---
+++ CovidTracker/CovidTracker/Helpers/ConvertNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace CovidTracker.Helpers
+{
+    public class ConvertNumber : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Decimal.Parse(value.ToString()).ToString("C");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string valueFromString = Regex.Replace(value.ToString(), @"\D", "");
+
+            if (valueFromString.Length <= 0)
+                return 0m;
+
+            long valueLong;
+            if (!long.TryParse(valueFromString, out valueLong))
+                return 0m;
+
+            if (valueLong <= 0)
+                return 0m;
+
+            return valueLong / 100m;
+        }
+    }
+}
