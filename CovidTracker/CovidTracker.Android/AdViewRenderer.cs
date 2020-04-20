dab06d48ab0ba3b2a28156c4f7e7b8725c00805f
Abraham Morillo <abrahammorillo777@gmail.com>
2020-04-19 22:33:03 -0400

    .

---
+++ CovidTracker/CovidTracker.Android/AdViewRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Ads;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CovidTracker.Controls;
+using CovidTracker.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+[assembly: Xamarin.Forms.ExportRenderer(typeof(AdControlView), typeof(AdViewRenderer))]
+namespace CovidTracker.Droid
+{
+    [Obsolete]
+    public class AdViewRenderer: ViewRenderer<Controls.AdControlView, AdView>
+    {
+        private AdView _adView;
+
+        public AdViewRenderer(Context context) : base(context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<AdControlView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null && e.NewElement != null)
+                SetNativeControl(CreateAdView());
+        }
+
+        private AdView CreateAdView()
+        {
+            if (_adView != null)
+                return _adView;
+
+            _adView = new AdView(Context)
+            {
+                AdUnitId = Element.AdUnitId,
+                AdSize = AdSize.SmartBanner
+            };
+
+            _adView.LayoutParameters = new LinearLayout.LayoutParams(LayoutParams.WrapContent, LayoutParams.WrapContent);
+
+            _adView.LoadAd(new AdRequest.Builder().Build());
+
+            return _adView;
+        }
+    }
+}
---
+++ CovidTracker/CovidTracker/Controls/AdControlView .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CovidTracker.Controls
+{
+    public class AdControlView : View
+    {
+        public static readonly BindableProperty AdUnitIdProperty = BindableProperty.Create(
+           nameof(AdUnitId),
+           typeof(string),
+           typeof(AdControlView));
+
+        public string AdUnitId
+        {
+            get => (string)GetValue(AdUnitIdProperty);
+            set => SetValue(AdUnitIdProperty, value);
+        }
+    }
+}
